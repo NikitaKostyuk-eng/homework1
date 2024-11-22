893be19451436fc4abe875538c9fa8751d4fe706
NikitaKostyuk-eng <kostuknikita987@gmail.com>
2024-11-22 17:17:57 +0300

    22/11

---
+++ main.py
@@ -0,0 +1,13 @@
+print(12 + 89 - 1)
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program"
+print('Задача 3 "Школьная загадка":')
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+# "4th program"
+print('4 "Первая цифра после запятой":')
+print(int(float('123.456') * 10) % 10)
